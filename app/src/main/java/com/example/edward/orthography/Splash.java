123a346ee1b22f3bad59d871a97a4d763c49a637
edward25nov <edward25nov@gmail.com>
2016-09-17 23:13:26 -0600

    subiendo la estructura inicial del proyecto

---
+++ app/src/main/java/com/example/edward/orthography/Splash.java
@@ -0,0 +1,41 @@
+package com.example.edward.orthography;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+public class Splash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        Animation ani = AnimationUtils.loadAnimation(this,R.anim.movercubo);
+        ImageView abc = (ImageView) findViewById(R.id.imgCubos);
+        abc.setAnimation(ani);
+
+        /*RotateAnimation anim = new RotateAnimation(0f, 350f, 15f, 15f);
+        anim.setInterpolator(new LinearInterpolator());
+        anim.setRepeatCount(Animation.INFINITE);
+        anim.setDuration(700);
+
+        abc.setAnimation(anim);*/
+
+        Handler h = new Handler();
+        h.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(Splash.this,MainActivity.class));
+                finish();
+            }
+        },4000);}
+
+
+
+}
+
