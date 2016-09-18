38cabefccd50a66bbbecefe4e35b709bed688143
edward25nov <edward25nov@gmail.com>
2016-09-18 02:07:39 -0600

    en fragment de amigos ya se puede cambiar de pantalla o fragmento

--- app/src/main/java/com/example/edward/orthography/AmigosFragment.java
+++ app/src/main/java/com/example/edward/orthography/AmigosFragment.java
@@ -25,14 +25,17 @@ public class AmigosFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_amigos,container,false);
+        final View view = inflater.inflate(R.layout.fragment_amigos,container,false);
         Button nuevaPartida = (Button) view.findViewById(R.id.btnNew);
 
         nuevaPartida.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
-
+            //http://stackoverflow.com/questions/36483010/onclick-button-event-in-fragment
+            //esta fue la que funciono :)
+            // http://stackoverflow.com/questions/27728439/implementing-onclicklistener-for-a-fragment-on-android
+             Toast.makeText(getActivity(), "Hello World", Toast.LENGTH_LONG).show();
+            // startActivity(new Intent(getActivity(),Splash.class));
             }
         });
 
