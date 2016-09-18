811d396556c770ba1b6520f0d2b143d96400746c
edward25nov <edward25nov@gmail.com>
2016-09-18 01:30:21 -0600

    fragmento de layout  agregado

---
+++ app/src/main/java/com/example/edward/orthography/AmigosFragment.java
@@ -0,0 +1,50 @@
+package com.example.edward.orthography;
+
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class AmigosFragment extends Fragment {
+
+
+    public AmigosFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_amigos,container,false);
+        Button nuevaPartida = (Button) view.findViewById(R.id.btnNew);
+
+        nuevaPartida.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+            }
+        });
+
+
+        return  view;
+
+        // Inflate the layout for this fragment
+      //  return inflater.inflate(R.layout.fragment_amigos, container, false);
+    }
+
+
+
+}
+
+
--- app/src/main/java/com/example/edward/orthography/MainActivity.java
+++ app/src/main/java/com/example/edward/orthography/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.edward.orthography;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.FragmentManager;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -12,6 +13,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TabHost;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -35,12 +37,47 @@ public class MainActivity extends AppCompatActivity
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-        drawer.setDrawerListener(toggle);
+        drawer.addDrawerListener(toggle);
         toggle.syncState();
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+        agregarPestañas();
+
+        AmigosFragment nuevoFragment = new AmigosFragment();
+        FragmentManager manager = getSupportFragmentManager();
+        manager.beginTransaction().replace(R.id.tab2,nuevoFragment,nuevoFragment.getTag()).commit();
+
+
+
     }
+
+    public void agregarPestañas(){
+        TabHost TbH = (TabHost) findViewById(R.id.tabHost); //llamamos al Tabhost
+        TbH.setup();                                       //lo activamos
+
+        TabHost.TabSpec tab1 = TbH.newTabSpec("tab1");  //aspectos de cada Tab (pestaña)
+        TabHost.TabSpec tab2 = TbH.newTabSpec("tab2");
+       // TabHost.TabSpec tab3 = TbH.newTabSpec("tab3");
+
+        tab1.setIndicator("LECCIONES");//qué queremos que aparezca en las pestañas
+        tab1.setContent(R.id.tab1); //definimos el id de cada Tab (pestaña)
+
+        tab2.setIndicator("AMIGOS");
+        tab2.setContent(R.id.tab2);
+
+       // tab3.setIndicator("TRES");
+        // tab3.setContent(R.id.tab3);
+
+        TbH.addTab(tab1); //añadimos los tabs ya programados
+        TbH.addTab(tab2);
+       // TbH.addTab(tab3);
+        TbH.setCurrentTab(0);
+
+
+    }
+
 
     @Override
     public void onBackPressed() {
@@ -82,6 +119,12 @@ public class MainActivity extends AppCompatActivity
 
         if (id == R.id.nav_camera) {
             // Handle the camera action
+
+            /***Reemplanzado el layout principal por un fragmento***/
+          /*  TabsFragment nuevoFragment = new TabsFragment();
+            FragmentManager manager = getSupportFragmentManager();
+            manager.beginTransaction().replace(R.id.Layout_a_ser_reemplazado,nuevoFragment,nuevoFragment.getTag()).commit();
+            */
         } else if (id == R.id.nav_gallery) {
 
         } else if (id == R.id.nav_slideshow) {
